569f3602be918fdfc40354e692631b4ca3e8e4e1
DeclanFitzpatrick <declan.fitzpatrick@ucalgary.ca>
2025-01-28 10:51:34 -0700

    plotting graph

---
+++ examples/driver_seismic.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    #load seismic data
+    data = np.loadtxt("C:/Users/Declan/repos/goph-w2025-lab01-stDF/data/s_wave_data.txt")
+    t_data = data[1,0]
+    v_data = data[1,1]
+    v2_data = v_data**2
+
+    plt.figure(figsize=(6,8))
+
+    plt.subplot(2, 1, 1)
+    plt.plot(t_data, v_data, "-b", label="s_wave_data")
+    plt.xlabel("time, t [s]")
+    plt.ylabel("velocity, v [mm/s]")
+    plt.legend
+
+    plt.subplot(2, 1, 2)
+    plt.plot(t_data, v_data, "-b", label="s_wave_data")
+    plt.xlabel("time, t [s]")
+    plt.ylabel("sq velocity, v**2 [mm/s]")
+    plt.legend()
+
+    plt.savefig("C:/Users/Declan/repos/goph-w2025-lab01-stDF/figures/s_wave_data.png")
+
+if __name__ == "__main__":
+    main()
